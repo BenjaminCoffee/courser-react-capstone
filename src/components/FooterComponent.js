6387822ccb5328325c9f44546e8d468a3495f676
Ben Pannell <benjamin7734@gmail.com>
2021-01-17 22:55:19 -0800

    Remade project, initial commit.

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,66 @@
+import React, {Component} from 'react';
+import {Col, Container, Row} from 'reactstrap';
+import {Link} from 'react-router-dom';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div className={'footer mb-5'}>
+                <Container>
+                    <Row className={'justify-content'}>
+                        <Col className={'col-4 offset-1 col-sm-2'}>
+                            <h5>Links</h5>
+                            <ul className="list-unstyled">
+                                <li><Link to={"/confusion/home"}>Home</Link></li>
+                                <li><Link to={"/confusion/aboutus"}>About</Link></li>
+                                <li><Link to={"/confusion/menu"}>Menu</Link></li>
+                                <li><Link to={"/confusion/contactus"}>Contact us</Link></li>
+                            </ul>
+                        </Col>
+                        <Col className={'col-7 col-sm-5'}>
+                            <h5>Our Address</h5>
+                            <address>
+                                121, Clear Water Bay Road<br/>
+                                Clear Water Bay, Kowloon<br/>
+                                HONG KONG<br/>
+                                <i className="fa fa-phone fa-lg"/>: +852 1234 5678<br/>
+                                <i className="fa fa-fax fa-lg"/>: +852 8765 4321<br/>
+                                <i className="fa fa-envelope fa-lg"/>: <a href="mailto:confusion@food.net">
+                                confusion@food.net</a>
+                            </address>
+                        </Col>
+                        <Col className={'col-12 col-sm-4 align-self-center'}>
+                            <div className="text-center">
+                                <a className="btn btn-social-icon btn-google" href="http://google.com/+">
+                                    <i className="fa fa-google-plus"/>
+                                </a>
+                                <a className="btn btn-social-icon btn-facebook"
+                                   href="http://www.facebook.com/profile.php?id=">
+                                    <i className="fa fa-facebook"/>
+                                </a>
+                                <a className="btn btn-social-icon btn-linkedin" href="http://www.linkedin.com/in/">
+                                    <i className="fa fa-linkedin"/>
+                                </a>
+                                <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/">
+                                    <i className="fa fa-twitter"/>
+                                </a>
+                                <a className="btn btn-social-icon btn-google" href="http://youtube.com/">
+                                    <i className="fa fa-youtube"/>
+                                </a>
+                                <a className="btn btn-social-icon" href="mailto:">
+                                    <i className="fa fa-envelope-o"/></a>
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row className={'justify-content'}>
+                        <Col className={'col-auto'}>
+                            <p>© Copyright 2018 Ristorante Con Fusion</p>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        )
+    }
+}
+
+export default Footer;
